6492c770d43152e30d9f8cebef70b7174fa955bd
Daniel Rugg <dfrugg@gmail.com>
2014-08-23 11:28:47 -0400

    Adding immutant

--- server/project.clj
+++ server/project.clj
@@ -6,4 +6,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.immutant/immutant "1.1.4"]
+                 [mysql/mysql-connector-java "5.1.32"]])
