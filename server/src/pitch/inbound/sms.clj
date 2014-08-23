31ab222b81c878704307adccf23cbb79cc2c1da3
Your Name <you@example.com>
2014-08-23 19:10:34 +0000

    determine-value bitches

--- server/src/pitch/inbound/sms.clj
+++ server/src/pitch/inbound/sms.clj
@@ -1,10 +1,22 @@
 (ns pitch.inbound.sms)
 
-(defn determine-command
+(defn poor-man-wordize
   [sms]
   (-> sms 
       :Body 
       clojure.string/trim
-      (.split "\\s+")
+      (.split "\\s+")))
+
+(defn determine-value
+  [sms]
+  (->> sms 
+       poor-man-wordize
+       rest
+       (clojure.string/join " ")))
+
+(defn determine-command
+  [sms]
+  (-> sms 
+      poor-man-wordize
       first
       clojure.string/lower-case))
