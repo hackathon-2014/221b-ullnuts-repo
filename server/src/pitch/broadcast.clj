656bc46792eb030321ad0c7b67710dd53b033a70
Daniel Rugg <dfrugg@gmail.com>
2014-08-23 13:40:59 -0400

    Adding twilio broadcast

---
+++ server/src/pitch/broadcast.clj
@@ -0,0 +1,27 @@
+(ns pitch.broadcast
+  (:require [twilio.core :as twilio]
+            [pitch.config :as config]
+            [clojure.core.async :refer :all]))
+
+(defn recipients
+  [to]
+  (if (coll? to)
+    to
+    [to]))
+
+
+(def message-channel (chan 10))
+
+(defn send-message
+  [to message]
+  (go 
+    (doseq [t (recipients to)]
+      (>! message-channel (twilio/sms (config/from) t message)))))
+
+(go 
+  (while true
+    (let [message (<! message-channel)]
+      (println "Attepting Send")
+      (twilio/with-auth (config/sid) (config/token)
+        (twilio/send message))
+      (println "Sent"))))
---
+++ server/src/pitch/config.clj
@@ -0,0 +1,11 @@
+(ns pitch.config )
+
+(def system-sid ((System/getProperty "twilio.sid")))
+(def system-token ((System/getProperty "twilio.token")))
+(def system-from ((System/getProperty "twilio.from")))
+
+(defn sid [] system-sid)
+
+(defn token [] system-token)
+
+(defn from [] system-from)
