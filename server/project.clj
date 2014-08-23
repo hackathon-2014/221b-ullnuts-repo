71b654e0f3176a064b03f0dd605aa02f4864e93c
Mark Gunnels <mark@mark-Lemur-Ultra.(none)>
2014-08-23 13:18:51 -0400

    I'm an idiot

--- server/project.clj
+++ server/project.clj
@@ -10,6 +10,7 @@
                  [org.immutant/immutant "1.1.4"]
                  [mysql/mysql-connector-java "5.1.32"]
                  [hiccup "1.0.5"]
+                 [compojure "1.1.8"]
                  [com.twilio.sdk/twilio-java-sdk "3.4.4"]]
   :plugins [[lein-ring "0.8.10"]]
   :ring {:handler pitch.handlers/app}
