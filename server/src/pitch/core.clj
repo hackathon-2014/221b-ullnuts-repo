50b1113578ae792e6d35647cc21656bd6727201e
Daniel Rugg <dfrugg@gmail.com>
2014-08-23 11:31:40 -0400

    Base files.

---
+++ server/src/pitch/core.clj
@@ -0,0 +1,6 @@
+(ns pitch.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ server/test/pitch/core_test.clj
@@ -0,0 +1,7 @@
+(ns pitch.core-test
+  (:require [clojure.test :refer :all]
+            [pitch.core :refer :all]))
+
+(deftest a-test
+  (testing "Who cares about tests.  I'll never fail."
+    (is (= 0 0))))
