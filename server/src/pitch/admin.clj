cad304e2e2b6fe22f6e024491e73467f6db0ce29
Daniel Rugg <dfrugg@gmail.com>
2014-08-23 16:38:17 -0400

    adding admin reactions

--- server/src/pitch/admin.clj
+++ server/src/pitch/admin.clj
@@ -1,22 +1,54 @@
 (ns pitch.admin
-  (:require [immutant.messaging :as messaging]))
+  (:require [immutant.messaging :as messaging]
+            [pitch.broadcast :as broadcast]
+            [pitch.inbound.sms :as sms]
+            [pitch.db :as db]))
+
+(def url "http://54.164.169.20:3000/​")
+
+
+(defn event-subscribers
+  [event]
+  ["+18436700979" "+18037385811"])
+
+(defn close-event
+  []
+  (close-active-events))
+
+(defn open-event
+  [message]
+  (db/set-active-event (sms/determine-value message))
+  message)
+
+(defn veto-pitch
+  [pitch-id]
+  )
 
 (defn handle-start
   [message]
-  (println "START: " message))
+  (-> message
+    open-event
+    :From
+    (broadcast/send-message (str "Event '" (sms/determine-value message) "' Has Started: " url)))
 
 
 (defn handle-end
   [message]
-  (println "END: " message))
+  (-> (close-event)
+    event-subscribers
+    (broadcast/send-message ("Event Has Closed: " url))))
 
 
 (defn handle-notify
   [message]
-  (println "NOTIFY: " message))
+  (-> (db/current-active-event)
+    event-subscribers
+    (broadcast/send-message (sms/determine-value message))))
 
 
 (defn handle-veto
   [message]
-  (println "VETO: " message))
+  (-> message
+    sms/determine-value
+    veto-pitch))
 
