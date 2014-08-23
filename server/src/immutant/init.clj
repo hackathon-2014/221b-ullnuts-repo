2346566d4a76ce85db90e61e1fa3d1ee99573fdf
Your Name <you@example.com>
2014-08-23 19:26:22 +0000

    Fixed Dan's shitty code

--- server/src/immutant/init.clj
+++ server/src/immutant/init.clj
@@ -3,9 +3,14 @@
             [immutant.web :as web]
             [immutant.jobs :as jobs]
             [immutant.repl :as repl]
-            [pitch.handlers :refer :all]))
+            [pitch.handlers :refer :all]
+            [pitch.admin :refer :all]))
 
 (web/start app)
 (messaging/start "topic.commands")
 
-(messaging/listen "topic.commands" println :selector "command='start'")
+(messaging/listen "topic.commands" println)
+(messaging/listen "topic.commands" handle-start :selector "command='start'")
+(messaging/listen "topic.commands" handle-end :selector "command='end'")
+(messaging/listen "topic.commands" handle-notify :selector "command='notify'")
+(messaging/listen "topic.commands" handle-veto :selector "command='veto'")
--- server/src/pitch/admin.clj
+++ server/src/pitch/admin.clj
@@ -20,7 +20,3 @@
   [message]
   (println "VETO: " message))
 
-(messaging/listen "topic.commands" handle-start :selector "command='start'")
-(messaging/listen "topic.commands" handle-end :selector "command='end'")
-(messaging/listen "topic.commands" handle-notify :selector "command='notify'")
-(messaging/listen "topic.commands" handle-veto :selector "command='veto'")
